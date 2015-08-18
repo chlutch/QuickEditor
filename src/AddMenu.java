307828292059e5cbd2036417c0f3f6095f5b6104
quickgrid <qdevapps@gmail.com>
2015-08-18 02:12:16 -0800

    Initial commit

---
+++ src/AddMenu.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.util.ResourceBundle;
+
+/**
+ * Created by asif on 8/18/2015.
+ */
+public class AddMenu {
+    JMenuBar jMenuBar;
+    JMenu fileMenu, viewMenu, helpMenu;
+    ResourceBundle menuResourceBundle;
+
+    public AddMenu(JMenuBar jMenuBar) {
+        this.jMenuBar = jMenuBar;
+        menuResourceBundle = ResourceBundle.getBundle("res.locale.Menus");
+        createComponentObjects();
+    }
+
+    public void createComponentObjects(){
+        fileMenu = new JMenu(menuResourceBundle.getString("FileMenu"));
+        viewMenu = new JMenu(menuResourceBundle.getString("ViewMenu"));
+    }
+
+    public void addComponents(){
+        jMenuBar.add(fileMenu);
+        jMenuBar.add(viewMenu);
+    }
+}
